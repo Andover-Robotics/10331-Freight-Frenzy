e42c9b17bd56d998150e589484b1b641659d26b1
SarahZhang <szhang2023@k12.andoverma.us>
2021-11-19 16:57:46 -0500

    added Intake subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/b_hardware/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/b_hardware/subsystems/Intake.java
@@ -2,7 +2,75 @@ package org.firstinspires.ftc.teamcode.b_hardware.subsystems;
 
 
 public class Intake extends SubsystemBase {
+    public static final double SPEED=0.5;
+    MotorEx leftIntake;
 
-//test commit
+    private enum state {
+        ON,
+        OFF,
+        REVERSE,
+        LEFT,
+        RIGHT
+    }
+
+    public state runState = state.OFF;
+
+    public Intake(OpMode opMode){
+        leftIntake = new MotorEx(opMode.hardwareMap, "leftIntake", Motor.GoBILDA.RPM_312);
+        leftIntake.setRunMode(Motor.RunMode.RawPower);
+        leftIntake.motor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        rightIntake = new MotorEx(opMode.hardwareMap, "rightIntake", Motor.GoBILDA.RPM_312);
+//        rightIntake.setRunMode(Motor.RunMode.RawPower);
+//        rightIntake.motor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+//
+    public void runToggle() {
+        if(runState == state.OFF) {
+            run();
+        }
+        else {
+            stop();
+        }
+    }
+
+    public void run(){
+        leftIntake.set(SPEED);
+//        rightIntake.set(SPEED);
+        runState = state.ON;
+    }
+
+//    public void reverse() {
+//        runState = state.REVERSE;
+//        leftIntake.set(-SPEED);
+////        rightIntake.set(-SPEED);
+//    }
+//
+//    public void runLeft(){
+////        rightIntake.set(0.0);
+//        leftIntake.set(SPEED);
+//        runState = state.LEFT;
+//    }
+
+//    public void runRight(){
+//        leftIntake.set(0.0);
+////        rightIntake.set(SPEED);
+//        runState = state.RIGHT;
+//    }
+
+//    public void switchIntake(){
+//        if (runState == state.LEFT) {
+//            runRight();
+//        }
+//        else {
+//            runLeft();
+//        }
+//    }
+
+    public void stop(){
+        leftIntake.set(0.0);
+//        rightIntake.set(0.0);
+        runState = state.OFF;
+    }
+}
 
 }
